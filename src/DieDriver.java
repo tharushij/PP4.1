284b66433835878b92ed15a445d1956bf48a7bb5
tharushij <tharushinatashaj@gmail.com>
2019-10-18 13:20:07 -0700

    Initial commit

---
+++ src/DieDriver.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class DieDriver
+{
+    public static void main(String[]args)
+    {
+        //Create some dice
+        Die die1 = new Die();
+        Die die2 = new Die();
+        //user input
+        Scanner keyboard = new Scanner(System.in);
+
+        //sides?
+        System.out.println("How many sides?");
+        numSides = keyboard.nextInt();
+
+        rollDie
+    }
+
+} //end class DieDriver
---
+++ src/rollDie.java
@@ -0,0 +1,15 @@
+public class rollDie
+{
+    private int numSides;
+
+    public rollDie()
+    {
+        numSides = 0;
+    }
+
+    public rollDie(numSides)
+    {
+        numSides = newNumSides;
+    }
+
+} //end class rollDie
